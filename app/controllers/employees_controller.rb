390aa0f1bfb69b954839f5e4e8e44ed36cfd520b
Thomas Stroemme <strtho@gmail.com>
2013-02-06 15:55:36 +0100

    adding google-analytics

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -44,7 +44,7 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully created.' }
         format.json { render json: @employee, status: :created, location: @employee }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.update_attributes(params[:employee])
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
+        format.html { redirect_to employees_path, notice: 'Employee was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
